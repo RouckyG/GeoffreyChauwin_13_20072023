76e1d0538d32b6ec033bdf90918a3c618928d169
Roucky <geoffrey.chauwin@etudiants.univ-reims.fr>
2023-08-11 09:59:19 +0200

    implement redux in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/argentBankLogo.png";
 
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from "../redux/user";
+
 const Header = (props) => {
+
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    
+    const handleLogout = () => {
+        dispatch(logout)
+    }
 
     return <nav className="main-nav">
         <NavLink to="/" className="main-nav-logo">
@@ -14,10 +24,23 @@ const Header = (props) => {
             <h1 className="sr-only">Argent Bank</h1>
         </NavLink>
         <div>
+        {user.isLogIn ? (
+          <>
+            <NavLink to="profile" className="main-nav-item">
+                <i className="fa fa-user-circle" />
+                {user.firstName}
+            </NavLink>
+            <NavLink to="/" className="main-nav-item" onClick={() => {handleLogout()}}>
+                <i className="fa fa-sign-out" />
+                Sign Out
+            </NavLink>
+          </>
+        ) : (
             <NavLink to="/login" className="main-nav-item">
-                <i className="fa fa-user-circle"></i>
+                <i className="fa fa-user-circle" />
                 Sign In
             </NavLink>
+        )}
         </div>
     </nav>
 }
